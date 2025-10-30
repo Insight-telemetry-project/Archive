08db47e2eca38a2509e0b239617df1aad739b10c
nadav5 <nadavbar205@gmail.com>
2025-10-30 13:07:32 +0200

    [MINOR] Add connections to schema

--- Archive/Models/Schema/TelemetryFlightData.cs
+++ Archive/Models/Schema/TelemetryFlightData.cs
@@ -10,5 +10,8 @@ namespace Archive.Models.Schema
 
         [BsonElement("Fields")]
         public Dictionary<string, int> Fields { get; set; } = new();
+        
+        [BsonElement("Connections")]
+        public Dictionary<string, List<string>> Connections { get; set; } = new();
     }
 }
